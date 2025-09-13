1db1b06eafff615bd20607bf15e9f1e43f60da99
Qwerty Fellow <chaitu.learnings.courses@gmail.com>
2025-09-12 22:28:47 -0700

    Remove tailwind

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Whereas disregard and contempt for human rights have resulted</h1>
+    <div className="bg-background p-6">
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, cum?</h1>
     </div>
   )
 }
